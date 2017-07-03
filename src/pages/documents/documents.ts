c7b13050d84fc421e5f3cb131860ce1d67ec03f8
seedgabo <seedgabo@gmail.com>
2017-07-03 11:18:54 -0500

    documents

--- src/pages/documents/documents.ts
+++ src/pages/documents/documents.ts
@@ -1,24 +1,32 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the DocumentsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
+import { NavController, NavParams } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Api } from "../../providers/api";
 @Component({
   selector: 'page-documents',
   templateUrl: 'documents.html',
 })
 export class DocumentsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  documents = [];
+  selected = null;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public api: Api, public sanitizer: DomSanitizer) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DocumentsPage');
+    this.getDocuments();
   }
-
+  getDocuments() {
+    this.api.get('documents')
+      .then((data: any) => {
+        console.log(data);
+        this.documents = data;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  download(document) {
+    console.log(document);
+    this.selected = document;
+  }
 }
