70f260950c4271f00880ae0ae582595a21fb646e
gabriel <seedgabo@gmail.com>
2017-08-22 07:02:01 -0500

    hooks for icons

--- src/providers/api.ts
+++ src/providers/api.ts
@@ -446,8 +446,8 @@ export class Api {
     this.Echo = undefined;
   }
 
-  pushRegister() {
-    this.onesignal.startInit("ebf07feb-3c76-4639-8c87-b1e7a2e9ddd8", "425679220353");
+  pushRegister(appid = "ebf07feb-3c76-4639-8c87-b1e7a2e9ddd8") {
+    this.onesignal.startInit(appid, "425679220353");
     this.onesignal.inFocusDisplaying(this.onesignal.OSInFocusDisplayOption.Notification);
     this.onesignal.syncHashedEmail(this.user.email);
     this.onesignal.sendTag("user_id", this.user.id);
