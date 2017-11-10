b2ba37dff3fa82c01cff4a11c5030264dfe1a193
seedgabo <seedgabo@gmail.com>
2017-11-09 21:39:41 -0500

    post changes

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -17,7 +17,7 @@ export class Login {
   ready = false;
   servers = { "0000": { "url": "http:\/\/residenciasonline.com\/residencias\/public\/", "name": "El Pe\u00f1on", "url_newton": "http:\/\/residenciasonline.com\/newton\/public" }, "1905": { "url": "http:\/\/residenciasonline.com\/aseinteg\/public\/", "name": "Aseinteg Especial", "url_newton": "http:\/\/residenciasonline.com\/newton\/public" }, "0001": { "url": "http:\/\/residenciasonline.com\/aseinteg\/public\/" }, "7000": { "url": "http:\/\/residenciasonline.com\/penon\/public\/", "name": "El Pe\u00f1on", "url_newton": "" }, "3720": { "url": "http:\/\/residenciasonline.com\/chestnut\/public\/", "name": "Prado Chestnut Hill", "url_newton": "" } };
   code = "";
-  preconfigure = false;
+  preconfigured = false;
   constructor(public facebook: Facebook, public google: GooglePlus, public navCtrl: NavController, public navParams: NavParams, public api: Api, public alertCtrl: AlertController, public loadingCtrl: LoadingController, public events: Events) {
     if (window.url) {
       this.preconfigured = true;
