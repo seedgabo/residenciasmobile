38068c9c6138f2648003d67de25be6ea5f8d68e7
seedgabo <seedgabo@gmail.com>
2017-11-26 00:22:34 -0500

    ticket ready

--- src/pages/ticket/ticket.ts
+++ src/pages/ticket/ticket.ts
@@ -1,3 +1,4 @@
+import { ActionSheetController } from 'ionic-angular/components/action-sheet/action-sheet-controller';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Api } from '../../providers/api';
@@ -10,48 +11,82 @@ export class TicketPage {
   ticket;
   new_comment = {
     text: "",
-    user_id: this.api.user.id
+    user_id: this.api.user.id,
+    ticket_id: null
   }
   loading = false;
-  adding =false;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public api:Api) {
+  adding = false;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public actionsheet: ActionSheetController, public api: Api) {
     this.ticket = navParams.get("ticket")
+    this.new_comment.ticket_id = this.ticket.id;
   }
 
   ionViewDidLoad() {
     this.getTicket();
   }
 
-  getTicket(){
+  getTicket() {
     this.loading = true;
     this.api.get(`tickets/${this.ticket.id}?with[]=comments&with[]=comments.user&with[]=comments.user.residence`)
-    .then((data)=>{
+      .then((data) => {
         this.loading = false;
         this.ticket = data;
       })
-      .catch((err)=>{
+      .catch((err) => {
         this.api.Error(err);
         this.loading = false;
-    })
+      })
   }
 
-  canAddComment(){
+
+  canAddComment() {
     return this.new_comment.text.length > 3;
   }
 
-  addComment(){
-      this.loading = true;
-      this.api.post("comments",this.new_comment)
-      .then((data)=>{
-          this.ticket.comments.push(data);
-          this.loading=false;
-          this.new_comment.text = "";
-          this.adding = false;
+  addComment() {
+    this.loading = true;
+    this.api.post("comments", this.new_comment)
+      .then((data: any) => {
+        data.user = this.api.user;
+        this.ticket.comments.push(data);
+        this.loading = false;
+        this.new_comment.text = "";
+        this.adding = false;
       })
-      .catch((err)=>{
+      .catch((err) => {
         this.api.Error(err);
-        this.loading=false;
+        this.loading = false;
       })
+  }
+
+  changeState(status = 'closed') {
+    this.api.put('tickets/' + this.ticket.id, { status: status })
+      .then((data) => {
+        this.ticket.status = status
+      })
+      .catch((err) => {
+        this.api.Error(err);
+      })
+  }
+
+  actions() {
+    var sheet = this.actionsheet.create({
+      title: this.api.trans('literals.action'),
+    });
+    sheet.addButton({
+      icon: 'checkmark-circle',
+      text: this.api.trans('literals.mark_as') + " " + this.api.trans('literals.done'),
+      handler: () => {
+        this.changeState('closed');
+      }
+    })
+    sheet.addButton({
+      icon: 'close',
+      text: this.api.trans('crud.cancel'),
+      handler: () => {
+      }
+    })
+    sheet.present();
   }
 
 }
--- src/pages/tickets/tickets.ts
+++ src/pages/tickets/tickets.ts
@@ -4,45 +4,49 @@ import { Api } from '../../providers/api';
 import { ActionSheetController } from 'ionic-angular/components/action-sheet/action-sheet-controller';
 import { ModalController } from 'ionic-angular/components/modal/modal-controller';
 @IonicPage({
-  defaultHistory:['HomePage']
+  defaultHistory: ['HomePage']
 })
 @Component({
   selector: 'page-tickets',
   templateUrl: 'tickets.html',
 })
 export class TicketsPage {
-  _tickets:any= {data:[]};
-  tickets= [];
+  _tickets: any = { data: [] };
+  tickets = [];
   query = "";
-  constructor(public navCtrl: NavController, public navParams: NavParams,public api:Api, public alert:AlertController, public actionsheet:ActionSheetController,public modal:ModalController) {
+  loading = false;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public api: Api, public alert: AlertController, public actionsheet: ActionSheetController, public modal: ModalController) {
   }
 
   ionViewDidLoad() {
-    this.api.ready.then(()=>{
+    this.api.ready.then(() => {
       this.getTickets();
     });
   }
 
-  getTickets(refresher=null){
+  getTickets(refresher = null) {
+    this.loading = true;
     this.api.get(`tickets?where[user_id]=${this.api.user.id}&withCount[]=comments&order[updated_at]=desc&paginate=100`)
-    .then((data:any)=>{
-      this._tickets = data;
-      this.filter();
-      if(refresher) refresher.complete()
-    })
-    .catch((err)=>{
-      this.api.Error(err);
-      if(refresher) refresher.complete()
-    })
+      .then((data: any) => {
+        this._tickets = data;
+        this.filter();
+        this.loading = false;
+        if (refresher) refresher.complete()
+      })
+      .catch((err) => {
+        this.api.Error(err);
+        this.loading = false;
+        if (refresher) refresher.complete()
+      })
   }
 
-  filter(){
-    if(this.query == ""){
+  filter() {
+    if (this.query == "") {
       this.tickets = this._tickets.data;
     }
   }
 
-  actions(ticket){
+  actions(ticket) {
     var sheet = this.actionsheet.create({
       title: this.api.trans('literals.actions'),
     });
@@ -50,49 +54,48 @@ export class TicketsPage {
       text: this.api.trans('literals.view_resource') + " " + this.api.trans("literals.ticket"),
       handler: () => {
         this.viewTicket(ticket)
-       }
+      }
     })
     sheet.addButton({
       text: this.api.trans('crud.edit') + " " + this.api.trans("literals.ticket"),
       handler: () => {
         this.editTicket(ticket)
-       }
+      }
     })
 
-    if(ticket.status != "closed" && ticket.status !== 'rejected'){
+    if (ticket.status != "closed" && ticket.status !== 'rejected') {
       sheet.addButton({
         text: this.api.trans('literals.mark_as') + " " + this.api.trans("literals.closed"),
-        handler: ()=>{}
+        handler: () => { }
       })
     }
 
     sheet.present()
   }
 
-  addTicket(){
+  addTicket() {
     var modal = this.modal.create("TicketEditorPage")
     modal.present();
-    modal.onWillDismiss((data)=>{
-      if(data && data.ticket){
-          this._tickets.push(data.ticket);
-          this.filter();
+    modal.onWillDismiss((data) => {
+      if (data && data.ticket) {
+        this._tickets.push(data.ticket);
+        this.filter();
       }
     })
   }
 
-  editTicket(ticket){
-    var modal = this.modal.create("TicketEditorPage",{ticket:ticket})
+  editTicket(ticket) {
+    var modal = this.modal.create("TicketEditorPage", { ticket: ticket })
     modal.present();
-    modal.onWillDismiss((data)=>{
-      if(data && data.ticket){
-          this._tickets.push(data.ticket);
-          this.filter();
+    modal.onWillDismiss((data) => {
+      if (data && data.ticket) {
+        ticket = data.ticket;
+        this.filter();
       }
     })
   }
-
-  viewTicket(ticket){
-    this.navCtrl.push("TicketPage",{ticket:ticket})
+  viewTicket(ticket) {
+    this.navCtrl.push("TicketPage", { ticket: ticket })
   }
 
 }
