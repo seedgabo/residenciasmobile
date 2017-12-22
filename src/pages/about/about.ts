4dfe51190079e94f330f5780914bfeaaa91ef456
gabriel <seedgabo@gmail.com>
2017-12-22 15:45:08 -0500

    about fixed

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CodePush } from '@ionic-native/code-push';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { ToastController } from 'ionic-angular';
 declare var window: any;
 @IonicPage()
 @Component({
@@ -12,7 +14,7 @@ export class AboutPage {
   live
   description
   ready = false
-  constructor(public navCtrl: NavController, public navParams: NavParams, public codepush: CodePush) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public codepush: CodePush, public splashScreen: SplashScreen, public toast: ToastController) {
   }
 
   ionViewDidLoad() {
@@ -31,6 +33,28 @@ export class AboutPage {
         this.live = window.live_version || "--";
         console.warn(err);
       })
+  }
+
+  sync(refresher = null) {
+    if (refresher) refresher.complete()
+    var sync = () => {
+      this.codepush.sync({ updateDialog: false, ignoreFailedUpdates: false, }).subscribe(
+        (status) => {
+          console.log(status)
+          if (status == 6) {
+            var msg = "Downloading Update";
+            this.toast.create({
+              message: msg,
+              duration: 3000,
+            }).present()
+          }
+          if (status == 8)
+            this.splashScreen.show();
+        }
+        , (err) => { console.warn(err) });
+
+    }
+    sync();
   }
 
 }
