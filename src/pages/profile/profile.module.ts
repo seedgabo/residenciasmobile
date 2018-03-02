635f0c8ac69f45f6a54196dafb0cfc864d7b17dd
gabriel <seedgabo@gmail.com>
2018-03-02 13:15:43 -0500

    remove round progress

--- src/pages/profile/profile.module.ts
+++ src/pages/profile/profile.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ProfilePage } from './profile';
 import { PipesModule } from '../../pipes/pipes.module';
-import { RoundProgressModule } from 'angular-svg-round-progressbar';
-
 @NgModule({
   declarations: [ProfilePage],
   imports: [
     IonicPageModule.forChild(ProfilePage),
     PipesModule,
-    RoundProgressModule,
   ],
 })
 export class ProfilePageModule { }
