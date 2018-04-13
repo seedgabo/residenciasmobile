a77b068234ef78a21090fd3244a07ce0ee790492
gabriel <seedgabo@gmail.com>
2018-04-13 08:29:34 -0500

    e

--- src/components/survey/survey.ts
+++ src/components/survey/survey.ts
@@ -10,7 +10,24 @@ var funct;
 export class SurveyComponent implements AfterViewInit, OnDestroy {
   vote;
   _survey;
+  data = [];
+  labels = [];
   chart;
+  colors = [
+    "#2196F3",
+    "#F44336",
+    "#FFC107",
+    "#4CAF50",
+    "#9C27B0",
+    "#E91E63",
+    "#E67E22",
+    "#82E0AA",
+    "#212F3C",
+    "#D7DBDD",
+    "#85C1E9",
+    "#F9E79F",
+    "#C0392B"
+  ];
   @Input()
   set survey(survey: any) {
     this._survey = survey;
@@ -30,7 +47,7 @@ export class SurveyComponent implements AfterViewInit, OnDestroy {
     this.renderChart();
     this.getVote();
     funct = (data) => {
-      this.update();
+      this.surveyUpdated(data);
     };
     this.events.subscribe("survey:updated", funct);
   }
@@ -47,52 +64,28 @@ export class SurveyComponent implements AfterViewInit, OnDestroy {
 
   total() {
     var sum = 0;
-    sum += parseInt(this._survey.results[1] ? this._survey.results[1] : 0);
-    sum += parseInt(this._survey.results[2] ? this._survey.results[2] : 0);
-    sum += parseInt(this._survey.results[3] ? this._survey.results[3] : 0);
-    sum += parseInt(this._survey.results[4] ? this._survey.results[4] : 0);
-    sum += parseInt(this._survey.results[5] ? this._survey.results[5] : 0);
-    sum += parseInt(this._survey.results[6] ? this._survey.results[6] : 0);
-
+    Object.keys(this.survey.results).forEach((key) => {
+      sum += this.survey.results[key] ? parseInt(this.survey.results[key]) : 0;
+    });
     return sum;
   }
 
   renderChart() {
     var ctx: any = document.getElementById("surveyChart");
     ctx = ctx.getContext("2d");
-    var data = [];
-    var labels = [];
-    if (this._survey.response_1) {
-      data.push(this._survey.results[1]);
-      labels.push(this._survey.response_1);
-    }
-    if (this._survey.response_2) {
-      data.push(this._survey.results[2]);
-      labels.push(this._survey.response_2);
-    }
-    if (this._survey.response_3) {
-      data.push(this._survey.results[3]);
-      labels.push(this._survey.response_3);
-    }
-    if (this._survey.response_4) {
-      data.push(this._survey.results[4]);
-      labels.push(this._survey.response_4);
-    }
-    if (this._survey.response_5) {
-      data.push(this._survey.results[5]);
-      labels.push(this._survey.response_5);
-    }
-    if (this._survey.response_6) {
-      data.push(this._survey.results[6]);
-      labels.push(this._survey.response_6);
-    }
+    var data = (this.data = []);
+    var labels = (this.labels = []);
+    Object.keys(this._survey.results).forEach((key) => {
+      data.push(this._survey.results[key]);
+      labels.push(key);
+    });
     this.chart = new Chart(ctx, {
       type: "pie",
       data: {
         datasets: [
           {
             data: data,
-            backgroundColor: ["#2196F3", "#F44336", "#FFC107", "#4CAF50", "#9C27B0", "#E91E63"],
+            backgroundColor: data.length > 16 ? this.colors.concat(this.colors) : this.colors,
             label: this.api.trans("__.# of Votes")
           }
         ],
@@ -105,49 +98,20 @@ export class SurveyComponent implements AfterViewInit, OnDestroy {
   }
 
   setVote() {
+    let inputs = [];
+    this._survey.responses.forEach((response) => {
+      inputs.push({
+        label: response.response,
+        type: "radio",
+        value: response.response,
+        name: "response"
+      });
+    });
     this.alert
       .create({
         title: this._survey.question,
         subTitle: this._survey.question,
-        inputs: [
-          {
-            label: this._survey.response_1,
-            type: "radio",
-            value: "1",
-            name: "response",
-            checked: true
-          },
-          {
-            label: this._survey.response_2,
-            type: "radio",
-            value: "2",
-            name: "response"
-          },
-          {
-            label: this._survey.response_3,
-            type: "radio",
-            value: "3",
-            name: "response"
-          },
-          {
-            label: this._survey.response_4,
-            type: "radio",
-            value: "4",
-            name: "response"
-          },
-          {
-            label: this._survey.response_5,
-            type: "radio",
-            value: "5",
-            name: "response"
-          },
-          {
-            label: this._survey.response_6,
-            type: "radio",
-            value: "6",
-            name: "response"
-          }
-        ],
+        inputs: inputs,
         buttons: [
           {
             text: this.api.trans("literals.vote"),
