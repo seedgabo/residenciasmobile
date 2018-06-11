d611e292b5fe1822f4554fbe2c7f292515d8a5cf
gabriel <seedgabo@gmail.com>
2018-06-11 15:13:42 -0500

    cuenta de cobro

--- src/pages/invoices/invoices.ts
+++ src/pages/invoices/invoices.ts
@@ -1,11 +1,5 @@
 import { Component } from "@angular/core";
-import {
-  NavController,
-  NavParams,
-  ActionSheetController,
-  PopoverController,
-  Platform
-} from "ionic-angular";
+import { NavController, NavParams, ActionSheetController, PopoverController, Platform } from "ionic-angular";
 import { Api } from "../../providers/api";
 import { FileTransfer, FileTransferObject } from "@ionic-native/file-transfer";
 import { File } from "@ionic-native/file";
@@ -13,6 +7,7 @@ import { FileOpener } from "@ionic-native/file-opener";
 
 import { IonicPage } from "ionic-angular";
 import moment from "moment";
+import { PopoverMenu } from "../popover/popover-menu";
 moment.locale("es");
 @IonicPage()
 @Component({
@@ -42,7 +37,8 @@ export class InvoicesPage {
     public file: File,
     public fileOpener: FileOpener,
     public actionsheet: ActionSheetController,
-    public popover: PopoverController
+    public popover: PopoverController,
+    public popovermenu: PopoverMenu
   ) {
     this.mobile = this.platform.is("android") || this.platform.is("ios");
   }
@@ -56,9 +52,7 @@ export class InvoicesPage {
   getInvoices(refresher = null) {
     this.api
       .get(
-        `invoices?order[date]=desc&orWhere[residence_id]=${
-          this.api.user.residence_id
-        }&orWhere[user_id]=${
+        `invoices?order[date]=desc&orWhere[residence_id]=${this.api.user.residence_id}&orWhere[user_id]=${
           this.api.user.id
         }&with[]=user&with[]=receipts&with[]=items&take=500`
       )
@@ -69,7 +63,7 @@ export class InvoicesPage {
         this.loading = false;
         if (refresher != null) refresher.complete();
       })
-      .catch(err => {
+      .catch((err) => {
         console.error(err);
         if (refresher != null) refresher.complete();
         this.loading = false;
@@ -81,7 +75,7 @@ export class InvoicesPage {
       this.toShow = this.api.objects.invoices;
       return this.getRows();
     }
-    this.toShow = this.api.objects.invoices.filter(inv => {
+    this.toShow = this.api.objects.invoices.filter((inv) => {
       return inv.type == this.types;
     });
     this.getRows();
@@ -89,7 +83,7 @@ export class InvoicesPage {
 
   getRows() {
     var arr = [];
-    this.toShow.forEach(inv => {
+    this.toShow.forEach((inv) => {
       arr.push({
         invoice: inv,
         id: inv.id,
@@ -117,11 +111,11 @@ export class InvoicesPage {
             "Auth-Token": this.api.user.token
           }
         })
-        .then(entry => {
+        .then((entry) => {
           console.log(entry);
           this._openFile(entry.toURL());
         })
-        .catch(err => {
+        .catch((err) => {
           console.error(err);
         });
     } else {
@@ -130,12 +124,11 @@ export class InvoicesPage {
   }
 
   downloadInvoiceBrowser(invoice) {
-    var url =
-      this.api.url +
-      "api/invoice/" +
-      invoice.id +
-      "/pdf?pdf=1&token=" +
-      this.api.user.token;
+    var url = this.api.url + "api/invoice/" + invoice.id + "/pdf?pdf=1&token=" + this.api.user.token;
+    window.open(url, "invoice");
+  }
+  downloadchargeAccount() {
+    var url = this.api.url + `api/charge_account/${this.api.user.residence_id}/pdf&token=${this.api.user.token}`;
     window.open(url, "invoice");
   }
 
@@ -152,11 +145,11 @@ export class InvoicesPage {
             "Auth-Token": this.api.user.token
           }
         })
-        .then(entry => {
+        .then((entry) => {
           console.log(entry);
           this._openFile(entry.toURL());
         })
-        .catch(err => {
+        .catch((err) => {
           console.error(err);
         });
     } else {
@@ -166,12 +159,7 @@ export class InvoicesPage {
   }
 
   downloadReceiptBrowser(receipt) {
-    var url =
-      this.api.url +
-      "api/receipt/" +
-      receipt.id +
-      "/pdf?pdf=1&token=" +
-      this.api.user.token;
+    var url = this.api.url + "api/receipt/" + receipt.id + "/pdf?pdf=1&token=" + this.api.user.token;
     window.open(url, "receipt");
   }
 
@@ -179,7 +167,7 @@ export class InvoicesPage {
     this.fileOpener
       .open(url, "application/pdf")
       .then(() => console.log("File is opened"))
-      .catch(e => console.log("Error openening file", e));
+      .catch((e) => console.log("Error openening file", e));
   }
 
   viewInvoice(invoice) {
@@ -187,11 +175,7 @@ export class InvoicesPage {
   }
 
   reportPayment(invoice) {
-    var popover = this.popover.create(
-      "PaymentReportPage",
-      { invoice: invoice },
-      { cssClass: "popover-payment" }
-    );
+    var popover = this.popover.create("PaymentReportPage", { invoice: invoice }, { cssClass: "popover-payment" });
     popover.present();
     popover.onWillDismiss(() => {
       this.getInvoices();
@@ -202,17 +186,14 @@ export class InvoicesPage {
     if (confirm(this.api.trans("__.are you sure?")))
       this.api
         .post(`invoice/${invoice.id}/email`, {})
-        .then(data => {})
+        .then((data) => {})
         .catch(console.error);
   }
 
   openMenu(invoice) {
     var buttons = [
       {
-        text:
-          this.api.trans("literals.download") +
-          " " +
-          this.api.trans("literals.invoice"),
+        text: this.api.trans("literals.download") + " " + this.api.trans("literals.invoice"),
         icon: "document",
         cssClass: "danger",
         handler: () => {
@@ -220,10 +201,7 @@ export class InvoicesPage {
         }
       },
       {
-        text:
-          this.api.trans("literals.view_resource") +
-          " " +
-          this.api.trans("literals.details"),
+        text: this.api.trans("literals.view_resource") + " " + this.api.trans("literals.details"),
         icon: "ios-list-box",
         cssClass: "",
         handler: () => {
@@ -234,10 +212,7 @@ export class InvoicesPage {
 
     if (invoice.receipts && invoice.receipts.length > 0) {
       buttons.push({
-        text:
-          this.api.trans("literals.download") +
-          " " +
-          this.api.trans("literals.receipt"),
+        text: this.api.trans("literals.download") + " " + this.api.trans("literals.receipt"),
         icon: "filing",
         cssClass: "secondary",
         handler: () => {
@@ -263,5 +238,39 @@ export class InvoicesPage {
     });
 
     sheet.present();
+  }
+
+  presentPopover(Ev) {
+    var buttons = [
+      {
+        text: this.api.trans("literals.download") + " " + this.api.trans("literals.charge account"),
+        icon: "document",
+        handler: () => {
+          if (this.platform.is("android") || this.platform.is("ios")) {
+            var transfer: FileTransferObject = this.transfer.create();
+            var url = this.api.url + `api/charge_account/${this.api.user.residence_id}/pdf`;
+            transfer
+              .download(url, this.file.dataDirectory + "charge_account.pdf", true, {
+                headers: {
+                  "Auth-Token": this.api.user.token
+                }
+              })
+              .then((entry) => {
+                console.log(entry);
+                this._openFile(entry.toURL());
+              })
+              .catch((err) => {
+                console.error(err);
+              });
+          } else {
+            return this.downloadchargeAccount();
+          }
+        }
+      }
+    ];
+    let popover = this.popovermenu.create({
+      buttons: buttons
+    });
+    popover.present({ ev: Ev });
   }
 }
